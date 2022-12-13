cf35a279b09a7683b4f851450c5529b3e0b54594
Chaitra P <chaitra.p@bp.com>
2022-12-13 07:53:19 +0530

    Main method

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //PrintUsername.Display();
+
+            //Console.WriteLine(PrintUsername.a);
+
+            FlipCoin.FlipTheCoin();
+
+        }
+
+    }
+}
